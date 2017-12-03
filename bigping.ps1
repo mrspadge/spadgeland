1954acdc0c1605aea0bd574c3bfab2ca068b400d
mrspadge <30719224+mrspadge@users.noreply.github.com>
2017-12-03 17:09:48 +0000

    Add files via upload

---
+++ bigping.ps1
@@ -0,0 +1,17 @@
+$servers = Get-Content C:\Scripts\machinestoping.txt
+$collection = $()
+foreach ($server in $servers)
+{
+	$status= @{ "ServerName" = $server; "TimeStamp" = (Get-Date -f s) }
+	if (Test-Connection $server -Count 1 -ea 0 -Quiet)
+	{
+		$status["Results"] = "Up"
+	}
+	else
+	{	
+		$status["Results"] = "Down"
+	}
+	New-Object -TypeName PSObject -Property $Status -OutVariable serverStatus
+	$collection += $serverStatus
+}
+$collection | Out-file C:\BigPingClientStatus.txt
